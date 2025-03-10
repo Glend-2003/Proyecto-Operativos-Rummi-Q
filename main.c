1efc2977dc515b416f5fe0f02b428552174e2b58
Jeremy Romero Carazo <117392636+Romero42@users.noreply.github.com>
2025-03-09 19:26:27 -0600

    Delete main.c

--- main.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>  // Para funciones de entrada/salida como printf
-#include <stdlib.h> // Para funciones estándar como exit#include <stdio.h>
-
-int main() {
-    printf("Hola, mundo!\n");
-    return 0;
-}
