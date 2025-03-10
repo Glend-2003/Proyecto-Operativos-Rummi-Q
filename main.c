3a4c674fe3f260769d54fd822a23adfe2b9d9939
Glend <115124841+Glend-2003@users.noreply.github.com>
2025-03-09 19:12:08 -0600

    Funcionando main
    Hola mundo y json necesario para librerias

---
+++ main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>  // Para funciones de entrada/salida como printf
+#include <stdlib.h> // Para funciones estándar como exit#include <stdio.h>
+
+int main() {
+    printf("Hola, mundowdadawdawddcad!\n");
+    return 0;
+}
